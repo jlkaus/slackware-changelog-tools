a693e3e5d03094ae1b4ac4f2913e7eb7bc636354
Jonathan L. Kaus <jlkaus@gmail.com>
2022-02-12 14:30:36 -0600

    Clean out old crap in preparation for splitting the repo.

--- fetch-pkg.pl
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-PKGNAME=${1:?Need to specify a package path to download.}
-VERSION=${2:-current}
-
-cd /tmp
-wget -q ftp://ftp.slackware.com/pub/slackware/slackware64-${VERSION}/slackware64/${PKGNAME}
